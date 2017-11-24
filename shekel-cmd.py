441168cf0f385e39d1020ec6f519f7036022fec2
Keybinder <jonahhopkin@gmail.com>
2017-11-24 21:58:32 +1100

    Added file for command-line version of shekel

---
+++ shekel-cmd.py
@@ -0,0 +1,35 @@
+# Shekel command-line v0.0.1
+
+import shekel
+
+def display_board(board, x=1, y=0, gridref=True, compact=False): #add colours? orientation
+
+     gridnums = [i for i in range(8,0,-1)]
+     gridletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+     
+     for i in range(8):
+          if gridref == True:
+               line = str(gridnums[i]) + "|"
+          elif gridref == False:
+               line = ""
+          
+          for j in board[i]:
+               line = line + (x * ' ') + j
+          if gridref == False or y == 0:
+               print(line + (y * '\n'))
+          else:
+               print(line + (y * '\n |'))
+               # pretty sure this shouldn't work but it does
+          
+     if gridref == True:
+          print((" +") + ('-' * (7 + 7 * x)))
+          lastline = "  "
+          for i in gridletters:
+               lastline = lastline + (x * ' ') + i
+          print(lastline)
+
+     if compact == False:
+          print('')
+
+shekel_board = shekel.set_board()
+display_board(shekel_board)
